1d7b9abc8d9f812db30d513d7ce58d048d2102b8
theo-lemoine-oc <propjhh>
2023-01-15 22:27:49 +0100

    create banner hero

---
+++ src/components/utils/Paragraph.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Paragraph = (props) => {
+    return (
+        <p className='text-gray-600 mt-8 font-semibold'>{ props.content }</p>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import sysadmin from '../assets/img/sysadmin.png';
+import { Paragraph } from '../components/utils/Paragraph';
 
 function Home() {
 
   return (
-    <div>
-        <h1>Accueil</h1>
+    <div className='flex items-center justify-between'>
+      <div className='flex flex-col justify-center w-1/2'>
+        <h1 className='text-transparent text-9xl bg-clip-text bg-gradient-to-r from-[#f0562f] to-[#d300c1]'>Hugo Lelong</h1>
+        <Paragraph content="
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium ex officiis quasi voluptatibus fuga dolor, in ipsam dolorum temporibus voluptas maiores, 
+          doloremque perspiciatis possimus repellendus alias. Reiciendis qui libero enim."
+        />
+      </div>
+      <div className='flex justify-center w-1/2'>
+        <img src={ sysadmin } alt="" className='w-[650px]' />
+      </div>
     </div>
   );
 }

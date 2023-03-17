88acafaf661eb36553a1065ddf6a9c8e3ba0c153
theo-lemoine-oc <propjhh>
2023-03-17 20:09:03 +0100

    create 404 page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Error() {
 
   return (
-    <div>
-        <h1>Page introuvable</h1>
-    </div>
+    <section className='h-screen flex flex-col items-center justify-center px-8'>
+      <h1 className='text-[10rem] laptop:text-[15rem] text-red-500'>404</h1>
+      <h2 className='text-3xl text-red-500 text-center'>La page que vous avez recherché est introuvable.</h2>
+      <Link to="/" className='mt-8 text-2xl bg-red-500 text-white py-2 px-10 rounded-2xl font-semibold text-center hover:bg-red-600 duration-200 ease'>Revenir vers l'accueil</Link>
+    </section>
   );
 }
 
